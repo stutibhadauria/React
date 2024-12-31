962bee4265ea522040394ea6c7d1b4a68cde9c5e
stutibhadauria <stutibhadauria28@gmail.com>
2024-12-31 17:34:31 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import A from './component/A'
+import B from './component/B'
+import C from './component/C'
+import D from './component/D'
+import E from './component/E'
+
+function App() {
+  return (
+    <>
+    <A/>
+    <B/>
+    <C/>
+    <D/>
+    <E/>
+    </>
+  )
+}
+
+export default App
